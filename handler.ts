37571d4ec9ef06f6d02149eba984b23eeeb3f9d8
gywls517 <gywls517@naver.com>
2020-04-03 13:08:07 +0900

    Initial commit

---
+++ handler.ts
@@ -0,0 +1,10 @@
+//import CrawlerController from './controllers/Crawl';
+
+export const handleRequest = async (url: string) => {
+  //const option = await CrawlerController.request(url);
+  //console.log(option);
+
+  return {
+    url
+  };
+};
