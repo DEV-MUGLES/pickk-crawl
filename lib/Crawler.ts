daefbd48734c2b0bf4f2e81079c6a1863ef96179
greatSumini <greatsumini@gmail.com>
2020-08-05 11:34:02 +0900

    feat: :sparkles: add isSoldout parse logic
    - select된 element가 displaynone class를 갖는가의 부정

--- lib/Crawler.ts
+++ lib/Crawler.ts
@@ -1,11 +1,11 @@
-import request from "request";
-import charset from "charset"; // 해당 사이트의 charset값을 알 수 있게 해준다.
-import { decode } from "iconv-lite";
-import axios from "axios";
+import request from 'request';
+import charset from 'charset'; // 해당 사이트의 charset값을 알 수 있게 해준다.
+import { decode } from 'iconv-lite';
+import axios from 'axios';
 
-import { strToNumber } from "./Parser";
-import { CrawlResult } from "types/Crawl";
-import { ISelecter } from "interfaces/ISelecter";
+import { strToNumber } from './Parser';
+import { CrawlResult } from 'types/Crawl';
+import { ISelecter } from 'interfaces/ISelecter';
 
 // crawl using request
 export const requestHtml = async (sourceUrl: string): Promise<string> => {
@@ -15,20 +15,20 @@ export const requestHtml = async (sourceUrl: string): Promise<string> => {
         url: sourceUrl, // 원하는 url값을 입력
         encoding: null, // 해당 값을 null로 해주어야 제대로 iconv가 제대로 decode 해준다.
         headers: {
-          "User-Agent":
-            "Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)", //            ''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+          'User-Agent':
+            'Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)', //            ''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
         },
       },
       async (error, res, body) => {
         if (!error && res.statusCode === 200 && body.length > 1000) {
           const enc = charset(res.headers, body); // 해당 사이트의 charset값을 획득
-          const iResult = decode(body, enc || "utf-8"); // 획득한 charset값으로 body를 디코딩
+          const iResult = decode(body, enc || 'utf-8'); // 획득한 charset값으로 body를 디코딩
           resolve(iResult);
         } else {
           const { data } = await axios.get(sourceUrl, {
             headers: {
-              "User-Agent":
-                "Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)",
+              'User-Agent':
+                'Mozilla/5.0 (compatible; Yeti/1.1; +http://naver.me/spd)',
             },
           });
           resolve(data);
@@ -49,31 +49,31 @@ export const selectAll = ($: CheerioStatic, selecter: ISelecter) => {
 
 export const select = ($: CheerioStatic, selecter: string): string => {
   try {
-    if (selecter.includes("meta")) {
+    if (selecter.includes('meta')) {
       return $(selecter).last().attr().content;
     }
-    if (selecter.includes("input")) {
+    if (selecter.includes('input')) {
       return $(selecter).last().val();
     }
-    if (selecter.includes("img")) {
+    if (selecter.includes('img')) {
       return $(selecter).last().attr().src;
     }
-    if (selecter[selecter.length - 1] === "a") {
+    if (selecter[selecter.length - 1] === 'a') {
       return $(selecter).last().attr().href;
     }
     return unescape($(selecter).last().text().trim());
   } catch {
-    return "";
+    return '';
   }
 };
 
 export const selectImages = ($: CheerioStatic, selecter: string): string[] => {
   const images = [];
-  $(selecter).each((i, ele) => {
+  $(selecter).each((_i, ele) => {
     images.push(
-      ele.attribs["src"] ||
-        ele.attribs["ec-data-src"] ||
-        ele.attribs["data-src"]
+      ele.attribs['src'] ||
+        ele.attribs['ec-data-src'] ||
+        ele.attribs['data-src']
     );
   });
   return images;
@@ -83,15 +83,18 @@ export const parseValue = (
   $: CheerioStatic,
   key: string,
   selecter: string
-): string | number | string[] => {
-  if (key === "images") {
+): string | number | string[] | boolean => {
+  if (key === 'images') {
     return selectImages($, selecter);
   }
+  if (key === 'isSoldout') {
+    return !$(selecter).hasClass('displaynone');
+  }
   const value = select($, selecter);
-  if (key === "name" && value[0] === "[" && value[value.length - 1] === "]") {
+  if (key === 'name' && value[0] === '[' && value[value.length - 1] === ']') {
     return value.slice(1, value.length - 1);
   }
-  if (key === "originalPrice" || key === "salePrice") {
+  if (key === 'originalPrice' || key === 'salePrice') {
     return strToNumber(value);
   }
   return value;
@@ -114,22 +117,22 @@ export const correct = (result: CrawlResult): CrawlResult => {
     originalPrice = salePrice;
     salePrice = temp;
   }
-  if (iu[0] === "/") {
-    imageUrl = "https:" + result.imageUrl;
+  if (iu[0] === '/') {
+    imageUrl = 'https:' + result.imageUrl;
   }
-  name = name.replace("[29CM단독] ", "");
+  name = name.replace('[29CM단독] ', '');
 
   return { ...result, name: name.trim(), imageUrl, originalPrice, salePrice };
 };
 
 export const getHostName = (url: string): string => {
   const { hostname } = new URL(url);
-  if (hostname.includes("topten10mall.com")) {
-    return "topten10mall.com";
+  if (hostname.includes('topten10mall.com')) {
+    return 'topten10mall.com';
   }
-  if (url.includes("smartstore.naver.com/juan_homme")) {
-    return "smartstore.naver.com/juan_homme";
+  if (url.includes('smartstore.naver.com/juan_homme')) {
+    return 'smartstore.naver.com/juan_homme';
   }
 
-  return hostname.replace("www.", "");
+  return hostname.replace('www.', '');
 };
