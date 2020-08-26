6f2070879c4db70d7da9d3d2c2643b201fb10c58
greatSumini <greatsumini@gmail.com>
2020-08-26 18:58:07 +0900

    feat(option): :sparkles: add josepht

--- services/option/crawlers.ts
+++ services/option/crawlers.ts
@@ -4,3 +4,15 @@ import { OptionResult } from 'types/option';
 export const cafe24 = (url: string, html: string): OptionResult => {
   return new OptionCralwer(url, html).cafe24().result;
 };
+
+export const _josephtcokr = (url: string, html: string): OptionResult => {
+  return new OptionCralwer(url, html)
+    .crawlOptionNames('div.item_add_option_box > dl > dt')
+    .crawlValues(
+      'div.item_add_option_box > dl > dd > select',
+      'option',
+      (ele) => ele.children[0].data.includes('품절'),
+      0,
+      1
+    ).result;
+};
