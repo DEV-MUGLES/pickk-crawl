4d1c3052b2336a0be004521a5508827f23e3f6fe
greatSumini <greatsumini@gmail.com>
2020-09-02 17:26:22 +0900

    feat(option): :sparkles: add OptionCrawl.phanties - sortie

--- services/option/index.ts
+++ services/option/index.ts
@@ -1,4 +1,7 @@
+import axios from 'axios';
+
 import * as crawlers from './crawlers';
+import phanties from './phanties';
 
 import { requestHtml, getHostName } from '../../lib';
 
@@ -12,6 +15,13 @@ export default class OptionCrawlService {
   }
 
   crawl = async (): Promise<any> => {
+    if (phanties.includes(getHostName(this.url))) {
+      console.log(this.url);
+      return axios
+        .get(`https://pickk-crawl.tk/option/?url=${this.url}`)
+        .then((res) => res.data);
+    }
+
     const html = await requestHtml(this.url);
     return (crawlers[this.crawlerName] || crawlers.cafe24)(this.url, html)
       .result;
