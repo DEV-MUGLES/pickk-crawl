9a2673f43bd86a840f1a30af2993fa97a9e7cdc2
greatSumini <greatsumini@gmail.com>
2020-11-17 00:59:02 +0900

    perf: :white_check_mark: seperate infoCrawl data fetch logic
    - data fetching logic만 global scope로 분리해 Promise.all로 병렬적으로 처리하도록 변경해 성능을 크게 높임

--- __tests__/services/info.test.ts
+++ __tests__/services/info.test.ts
@@ -1,12 +1,26 @@
 import InfoCrawlService from '../../services/info';
 import testCases from '../test-cases.json';
 
+let datas;
+beforeAll(async () => {
+  datas = await Promise.all(
+    testCases.map(
+      ({ url }) =>
+        new Promise(async (resolve) => {
+          const infoCrawlService = new InfoCrawlService(url);
+          const data = await infoCrawlService.crawl();
+          resolve(data);
+        })
+    )
+  );
+});
+
 describe('Test brands', () => {
-  for (const testCase of testCases) {
-    const { name, url, isPartner } = testCase;
-    it(name, async (done) => {
-      const infoCrawlService = new InfoCrawlService(url);
-      const data = await infoCrawlService.crawl();
+  for (let i = 0; i < testCases.length; ++i) {
+    const { name, isPartner } = testCases[i];
+    it(name, (done) => {
+      const data = datas[i];
+      expect(data).toBeTruthy();
       expect(data.brandKor.length).toBeGreaterThan(0);
       expect(data.name.length).toBeGreaterThan(0);
       expect(data.imageUrl.length).toBeGreaterThan(0);
