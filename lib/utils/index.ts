ec75e8f8d46a568fc1e979615022ae17724bcab3
greatSumini <greatsumini@gmail.com>
2020-11-19 03:56:44 +0900

    feat: :sparkles: add lib/utils.allSettled
    - Promise.allSettled의 polyfill

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -12,3 +12,22 @@ export const getAllCombination = (
     getAllCombination([...now, i], remain.slice(1), result);
   }
 };
+
+const isIterator = (obj) => {
+  return !!obj && !!obj[Symbol.iterator];
+};
+
+export const allSettled = (iterable) => {
+  if (!isIterator(iterable)) {
+    throw new Error('[allSettled] first param must be iterable');
+  }
+
+  const onFulfill = (v) => ({ status: 'fulfilled', value: v });
+  const onReject = (v) => ({ status: 'rejected', reason: v });
+
+  return Promise.all(
+    Array.from(iterable).map((p) =>
+      Promise.resolve(p).then(onFulfill).catch(onReject)
+    )
+  );
+};
