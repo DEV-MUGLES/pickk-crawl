bdf546ecf4fc82578269693c0c3321c7b048a131
greatSumini <greatsumini@gmail.com>
2021-02-16 19:09:04 +0900

    feat: :sparkles: add requestCookies

---
+++ lib/cookies.ts
@@ -0,0 +1,4 @@
+export const requestCookies = {
+  'maveworks.com':
+    '2a0d2363701f23f8a75028924a3af643=MjE4LjQ5LjIyNi4zMQ%3D%3D; PHPSESSID=juj55mv2r7altbd464njruv106; _ga=GA1.2.494630211.1613464802; _gid=GA1.2.96733555.1613464802; _fbp=fb.1.1613464803155.1078587594; 5b1ceb69146c0bafdc082ff42248da98=MTU5ODgzNjc1MQ%3D%3D; _gat_gtag_UA_187577754_2=1; wcs_bt=s_1ffa960b15e1:1613469220',
+};
