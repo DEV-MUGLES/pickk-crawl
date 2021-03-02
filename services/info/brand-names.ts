a4b4bbabed19f1e35ee3e29c335ca279443e9e9b
greatSumini <greatsumini@gmail.com>
2021-03-02 15:26:33 +0900

    feat(info): :sparkles: add guilf.co.kr

--- services/info/brand-names.ts
+++ services/info/brand-names.ts
@@ -271,6 +271,7 @@ export const brandNames = {
   'amekaji-moaa.com': '모아',
   'googongfast.com': '구공페스트',
   'nsad-official.com': 'NSAD*',
+  'guilf.co.kr': '길프',
 };
 
 // brandKorAlias의 key값 형식은 영문브랜드명에서 영문, 숫자 외 모든 문자를 제거한 소문자입니다.
