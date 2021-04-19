3498bd2d146623fd78637f7d9ef7b463f1a4224f
syoung125 <laura120555@gmail.com>
2021-04-19 09:55:20 +0900

    feat: :white_check_mark: replace data to faker data

--- services/info/index.spec.ts
+++ services/info/index.spec.ts
@@ -1,47 +1,49 @@
+import faker from 'faker';
+
 import { InfoResult } from 'types';
 import * as InfoCrawl from '.';
 
 const { formatResult } = InfoCrawl;
 
 describe('Test InfoCrawlService', () => {
-  const inputResult: InfoResult = {
-    name: '울 오버 싱글 코트 딥네이비',
-    brandKor: 'suare',
-    imageUrl: 'https://suare.diskn.com/1moTokNIXG',
-    originalPrice: 10000,
-    salePrice: 9000,
-  };
+  describe('formatResult', () => {
+    const [salePrice, originalPrice] = [
+      faker.random.number(),
+      faker.random.number(),
+    ].sort();
+    const inputResult: InfoResult = {
+      name: faker.commerce.productName(),
+      brandKor: faker.company.companyName(),
+      imageUrl: faker.image.imageUrl(),
+      originalPrice,
+      salePrice,
+    };
 
-  const host = 'suare.diskn.com';
-  const url =
-    'http://suare.co.kr/product/detail.html?product_no=1097&cate_no=24&display_group=1';
+    const host = 'suare.diskn.com';
+    const url = faker.image.imageUrl();
 
-  it('formatResult should return null when formatResult get null values', () => {
-    expect(formatResult(null, null, null)).toEqual(null);
-  });
+    it('should return null when formatResult get null values', () => {
+      expect(formatResult(null, null, null)).toEqual(null);
+    });
 
-  it("formatResult should't return null in images", () => {
-    const result = formatResult(
-      {
-        ...inputResult,
-        images: [
-          'https://suare.diskn.com/1moTokNIXG',
-          null,
-          'https://suare.diskn.com/1moTokNIXG',
-          null,
-        ],
-      },
-      host,
-      url
-    );
+    it("should't return null in images", () => {
+      const result = formatResult(
+        {
+          ...inputResult,
+          images: [faker.image.imageUrl(), null, faker.image.imageUrl(), null],
+        },
+        host,
+        url
+      );
 
-    expect(result.images).toEqual(expect.not.arrayContaining([null]));
-    expect(result.images.length).toEqual(2);
-  });
+      expect(result.images).toEqual(expect.not.arrayContaining([null]));
+      expect(result.images.length).toEqual(2);
+    });
 
-  it('formatResult result.isSoldout is false when result.isSoldout is null', () => {
-    const result = formatResult({ ...inputResult }, host, url);
+    it('result.isSoldout should be false when result.isSoldout is null', () => {
+      const result = formatResult({ ...inputResult }, host, url);
 
-    expect(result.isSoldout).toEqual(false);
+      expect(result.isSoldout).toEqual(false);
+    });
   });
 });
