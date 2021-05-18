d160b5e1a1058c17925678ba198bafa73e9cbbf1
greatSumini <greatsumini@gmail.com>
2021-05-18 17:38:56 +0900

    feat(info): :sparkles: add jerusalemsandalskorea.co.kr

--- services/info/brand-names.ts
+++ services/info/brand-names.ts
@@ -321,6 +321,7 @@ export const brandNames = {
   'tmaker.co.kr': '티메이커',
   'licflo.com': '릭플로',
   'areuban.com': '아르반',
+  'jerusalemsandalskorea.co.kr': '예루살렘샌들',
 };
 
 // brandKorAlias의 key값 형식은 영문브랜드명에서 영문, 숫자 외 모든 문자를 제거한 소문자입니다.
