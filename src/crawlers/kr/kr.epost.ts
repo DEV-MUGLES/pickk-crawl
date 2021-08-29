eb4dbcfa02fc558394ee694e4af42ecd8cd745f8
greatSumini <greatSumini@gmail.com>
2021-08-30 01:13:04 +0900

    feat(crawler): :art: refactor epost cralwer

--- src/crawlers/kr/kr.epost.ts
+++ src/crawlers/kr/kr.epost.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-import { XmlEntities as Entities } from 'html-entities';
-// import { JSDOM } from 'jsdom'
 import qs from 'querystring';
 
 import BaseCrawler from '../base';
 import { DELIVERED_STATUS_TEXT } from '../constants';
+import { ShippingInformation } from '../types';
 
 const parseStatus = (s: string) => {
   if (s.includes('집하완료')) return { id: 'at_pickup', text: '상품인수' };
@@ -28,111 +27,95 @@ export class KREpostCrawler extends BaseCrawler {
     tel: '+8215881300',
   };
 
-  crawl = () => {
-    return new Promise((resolve, reject) => {
+  crawl = () =>
+    new Promise(async (resolve, reject) => {
       try {
-        axios
-          .post(
-            'https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm',
-            qs.stringify({
-              sid1: this.trackingCode,
-            })
-          )
-          .then((res) => {
-            // const dom = new JSDOM(res.data)
-            // const document = dom.window.document
-
-            const $ = cheerio.load(res.data);
-
-            // const informationTable = document.querySelector('.table_col:nth-child(2)')
-            // const progressTable = document.querySelector('.table_col:nth-child(1)')
-
-            const $informationTable = $('.table_col:nth-child(2)');
-            const $progressTable = $('.table_col:nth-child(1)');
-
-            return { $, $informationTable, $progressTable };
+        const { data } = await axios.post(
+          'https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm',
+          qs.stringify({
+            sid1: this.trackingCode,
           })
-          .then(({ $, $informationTable, $progressTable }) => {
-            // const informations = informationTable.querySelectorAll('td')
-            const $informations = $informationTable.find('td');
-            const entities = new Entities();
+        );
+        const $ = cheerio.load(data.toString('utf-8'));
+        const [informationTable, progressTable] = Array.from($('tbody'));
 
-            const from = entities
-              .decode($informations.eq(0).html())
-              .split('<br>');
-            const to = entities
-              .decode($informations.eq(1).html())
-              .split('<br>');
-
-            // const from = informations[0].innerHTML.split('<br>')
-            // const to = informations[1].innerHTML.split('<br>')
-
-            if ($informations.length === 0) {
-              return reject({
-                code: 404,
-                message: '해당 운송장이 존재하지 않습니다.',
-              });
-            }
-
-            if ($informationTable.find('tr').length === 3) {
-              return reject({
-                code: 404,
-                message: parseTime(
-                  $informationTable.find('tr:nth-child(2)').eq(0).text()
-                ),
-              });
-            }
-
-            const shippingInformation = {
-              from: {
-                name: from[0],
-                time: from[0]
-                  ? `${from[1].replace(/\./g, '-')}T00:00:00+09:00`
-                  : '',
-              },
-              to: {
-                name: to[0],
-                time: to[0]
-                  ? `${parseTime(to[1].replace(/\./g, '-'))}T00:00:00+09:00`
-                  : '',
-              },
-              state: null,
-              progresses: [],
-            };
-
-            $progressTable.find('tr').each((_, element) => {
-              const td = $(element).find('td');
-              if (td.length === 0) {
-                return;
-              }
-              shippingInformation.progresses.push({
-                time: `${td.eq(0).html().replace(/\./g, '-')}T${td
-                  .eq(1)
-                  .html()}:00+09:00`,
-                location: {
-                  name: td.eq(2).find('a').eq(0).text(),
-                },
-                status: parseStatus(td.eq(3).text()),
-                description: parseTime(td.eq(3).text()),
-              });
-            });
-
-            if (shippingInformation.progresses.length > 0)
-              shippingInformation.state =
-                shippingInformation.progresses[
-                  shippingInformation.progresses.length - 1
-                ].status;
-            else
-              shippingInformation.state = {
-                id: 'information_received',
-                text: '방문예정',
-              };
-
-            resolve(shippingInformation);
+        if (!progressTable) {
+          return reject({
+            code: 404,
+            message: '운송장 정보를 찾을 수 없습니다.',
           });
+        }
+
+        const informationTable$ = $(informationTable);
+        const informationTableRowData$ = informationTable$
+          .children('tr')
+          .eq(0)
+          .children('td');
+        const shippingInformation: ShippingInformation = {
+          from: {
+            name: informationTableRowData$
+              .eq(0)
+              .text()
+              .trim()
+              .replace(/[0-9]|\./g, ''),
+            time: null,
+          },
+          to: {
+            name: informationTableRowData$
+              .eq(2)
+              .text()
+              .trim()
+              .replace(/[0-9]|\./g, ''),
+            time: null,
+          },
+          state: null,
+          progresses: [],
+        };
+
+        const progressTable$ = $(progressTable);
+        progressTable$.children('tr').each((_, element) => {
+          const td = $(element).children('td');
+          if (td.length === 0) {
+            return;
+          }
+          shippingInformation.progresses.push({
+            time: new Date(
+              `${td.eq(0).text().replace(/\./g, '-')}T${td
+                .eq(1)
+                .text()}:00+09:00`
+            ),
+            location: {
+              name: td.eq(2).children('a').eq(0).text(),
+            },
+            status: parseStatus(td.eq(3).text()),
+            description: parseTime(td.eq(3).text()),
+          });
+        });
+
+        const { progresses } = shippingInformation;
+        if (progresses.length > 0) {
+          shippingInformation.state = progresses[progresses.length - 1].status;
+          shippingInformation.from.time = progresses[0].time;
+
+          if (this.isDelivered(shippingInformation))
+            shippingInformation.to.time =
+              progresses[progresses.length - 1].time;
+        } else
+          shippingInformation.state = {
+            id: 'information_received',
+            text: '방문예정',
+          };
+
+        resolve(shippingInformation);
       } catch (err) {
         reject(err);
       }
     });
-  };
+
+  isDelivered(shippingInformation: ShippingInformation) {
+    const { progresses } = shippingInformation;
+    return (
+      progresses[progresses.length - 1].status.text === DELIVERED_STATUS_TEXT
+    );
+  }
 }
