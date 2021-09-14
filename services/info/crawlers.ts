10385a7853a4a55780fd0c804f999d9f7274bb8d
greatSumini <greatSumini@gmail.com>
2021-09-14 17:04:25 +0900

    fix(info): :bug: fix musinsa salePrice crawl logic

--- services/info/crawlers.ts
+++ services/info/crawlers.ts
@@ -20,11 +20,14 @@ export const _storemusinsacom = (
 
   const { name, brandKor } = result;
 
+  const salePrice = strToNumber(name.slice(name.lastIndexOf(' - ')))
+
   return {
     ...result,
-    name: name.slice(name.indexOf(') ') + 2, name.indexOf(' - ')),
+    name:  name.slice(name.indexOf(') ') + 2, name.indexOf(' - ')),
     brandKor: brandKor.slice(0, brandKor.indexOf('(')),
     isSoldout: $(selector.isSoldout).text() === '품절',
+    salePrice
   };
 };
 
